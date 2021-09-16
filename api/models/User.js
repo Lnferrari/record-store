000963685e5dcbf46b33f1fc06404860caa786be
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-09-16 13:43:42 +0200

    adding cart in the user model

--- api/models/User.js
+++ api/models/User.js
@@ -21,6 +21,10 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   role: { type: String, enum: ['user', 'admin'], default: 'user'},
   birthday: { type: Date },
+  cart: [{
+    type: Schema.Types.ObjectId,
+    required: false
+  }],
   address: { type: AddresSchema, required: true }
 },
 {
